42a83c34059da02411ea7b6fe369c6219fe8c40b
Evrim Diler <IS95217@gm.isbank.com.tr>
2018-11-23 17:48:01 +0300

    3rd commit

--- python/PPlate.py
+++ python/PPlate.py
@@ -107,9 +107,9 @@ def all_plates_classify(sess,tiger_test,net_inp,net_out):
         return nmatch_str,match_str,non_matched,matched
     
 ######################################        
-#               train model    
+#               train model     with 1 gru
 #####################################    
-def train(img_w,img_h,path_data,load,path_name_model,epoch_cnt):
+def train1gru(img_w,img_h,path_data,load,path_name_model,epoch_cnt):
     # Input Parameters
 
     # Network parameters
@@ -155,15 +155,229 @@ def train(img_w,img_h,path_data,load,path_name_model,epoch_cnt):
     gru_1 = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', name='gru1')(inner)
     gru_1b = GRU(rnn_size, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='gru1_b')(inner)
     gru1_merged = add([gru_1, gru_1b])
-    
-    
+        
+    """"
     gru_2 = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', name='gru2')(gru1_merged)
     gru_2b = GRU(rnn_size, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='gru2_b')(gru1_merged)
-    
+        
     # transforms RNN output to character activations:
     inner = Dense(tiger_train.get_output_size(), kernel_initializer='he_normal',
                   name='dense2')(concatenate([gru_2, gru_2b])) 
+    """
+    inner = Dense(tiger_train.get_output_size(), kernel_initializer='he_normal',
+                  name='dense2')(gru1_merged) 
+    
     y_pred = Activation('softmax', name='softmax')(inner)
+    
+    Model(inputs=input_data, outputs=y_pred).summary()
+    
+    labels = Input(name='the_labels', shape=[tiger_train.max_text_len], dtype='float32')
+    input_length = Input(name='input_length', shape=[1], dtype='int64')
+    label_length = Input(name='label_length', shape=[1], dtype='int64')
+    # Keras doesn't currently support loss funcs with extra parameters
+    # so CTC loss is implemented in a lambda layer
+    loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([y_pred, labels, input_length, label_length])
+    
+    # clipnorm seems to speeds up convergence
+    sgd = SGD(lr=0.02, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5)
+    
+    if load:
+        model = load_model('./tmp_model.h5', compile=False)
+    else:
+        model = Model(inputs=[input_data, labels, input_length, label_length], outputs=loss_out)
+
+    # the loss calc occurs elsewhere, so use a dummy lambda func for the loss
+    model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, 
+                  optimizer=sgd,
+                  metrics=['accuracy'])
+  
+    if not load:
+        # captures output of softmax so we can decode the output during visualization
+        test_func = K.function([input_data], [y_pred])
+       
+        
+        history= model.fit_generator(generator=tiger_train.next_batch(), 
+                            steps_per_epoch=tiger_train.n,
+                            epochs=epoch_cnt, 
+                            validation_data=tiger_val.next_batch(), 
+                            validation_steps=tiger_val.n)
+    
+    #save model**************
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open(path_name_model + ".json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights(path_name_model + ".h5")
+    print("Saved" + path_name_model + " model to disk") 
+    
+    return model
+#######################################
+
+def train2gru(img_w,img_h,path_data,load,path_name_model,epoch_cnt):
+    # Input Parameters
+
+    # Network parameters
+    conv_filters = 16
+    kernel_size = (3, 3)
+    pool_size = 2
+    time_dense_size = 32
+    rnn_size = 512
+
+    if K.image_data_format() == 'channels_first':
+        input_shape = (1, img_w, img_h)
+    else:
+        input_shape = (img_w, img_h, 1)
+        
+    batch_size = 32
+    downsample_factor = pool_size ** 2
+   
+    tiger_train = TextImageGenerator(path_data, 'train', img_w, img_h, batch_size, downsample_factor)
+    tiger_train.build_data()
+    tiger_val = TextImageGenerator(path_data, 'val', img_w, img_h, batch_size, downsample_factor)
+    tiger_val.build_data()
+   
+    
+    act = 'relu'
+    input_data = Input(name='the_input', shape=input_shape, dtype='float32')
+    inner = Conv2D(conv_filters, kernel_size, padding='same',
+                   activation=act, kernel_initializer='he_normal',
+                   name='conv1')(input_data)
+    inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max1')(inner)
+    inner = Conv2D(conv_filters, kernel_size, padding='same',
+                   activation=act, kernel_initializer='he_normal',
+                   name='conv2')(inner)
+    inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max2')(inner)
+
+    conv_to_rnn_dims = (img_w // (pool_size ** 2), (img_h // (pool_size ** 2)) * conv_filters)
+    inner = Reshape(target_shape=conv_to_rnn_dims, name='reshape')(inner)
+
+    # cuts down input size going into RNN:
+    inner = Dense(time_dense_size, activation=act, name='dense1')(inner)
+
+    # Two layers of bidirecitonal GRUs
+    # GRU seems to work as well, if not better than LSTM:
+    gru_1 = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', name='gru1')(inner)
+    gru_1b = GRU(rnn_size, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='gru1_b')(inner)
+    gru1_merged = add([gru_1, gru_1b])
+        
+  
+    gru_2 = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', name='gru2')(gru1_merged)
+    gru_2b = GRU(rnn_size, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='gru2_b')(gru1_merged)
+        
+    # transforms RNN output to character activations:
+    inner = Dense(tiger_train.get_output_size(), kernel_initializer='he_normal',
+                  name='dense2')(concatenate([gru_2, gru_2b]))     
+    
+    
+    y_pred = Activation('softmax', name='softmax')(inner)
+    
+    Model(inputs=input_data, outputs=y_pred).summary()
+    
+    labels = Input(name='the_labels', shape=[tiger_train.max_text_len], dtype='float32')
+    input_length = Input(name='input_length', shape=[1], dtype='int64')
+    label_length = Input(name='label_length', shape=[1], dtype='int64')
+    # Keras doesn't currently support loss funcs with extra parameters
+    # so CTC loss is implemented in a lambda layer
+    loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([y_pred, labels, input_length, label_length])
+    
+    # clipnorm seems to speeds up convergence
+    sgd = SGD(lr=0.02, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5)
+    
+    if load:
+        model = load_model('./tmp_model.h5', compile=False)
+    else:
+        model = Model(inputs=[input_data, labels, input_length, label_length], outputs=loss_out)
+
+    # the loss calc occurs elsewhere, so use a dummy lambda func for the loss
+    model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, 
+                  optimizer=sgd,
+                  metrics=['accuracy'])
+  
+    if not load:
+        # captures output of softmax so we can decode the output during visualization
+        test_func = K.function([input_data], [y_pred])
+       
+        
+        history= model.fit_generator(generator=tiger_train.next_batch(), 
+                            steps_per_epoch=tiger_train.n,
+                            epochs=epoch_cnt, 
+                            validation_data=tiger_val.next_batch(), 
+                            validation_steps=tiger_val.n)
+    
+    #save model**************
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open(path_name_model + ".json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights(path_name_model + ".h5")
+    print("Saved" + path_name_model + " model to disk") 
+    
+    return model
+####################################
+def train3gru(img_w,img_h,path_data,load,path_name_model,epoch_cnt):
+    # Input Parameters
+
+    # Network parameters
+    conv_filters = 16
+    kernel_size = (3, 3)
+    pool_size = 2
+    time_dense_size = 32
+    rnn_size = 512
+
+    if K.image_data_format() == 'channels_first':
+        input_shape = (1, img_w, img_h)
+    else:
+        input_shape = (img_w, img_h, 1)
+        
+    batch_size = 32
+    downsample_factor = pool_size ** 2
+   
+    tiger_train = TextImageGenerator(path_data, 'train', img_w, img_h, batch_size, downsample_factor)
+    tiger_train.build_data()
+    tiger_val = TextImageGenerator(path_data, 'val', img_w, img_h, batch_size, downsample_factor)
+    tiger_val.build_data()
+   
+    
+    act = 'relu'
+    input_data = Input(name='the_input', shape=input_shape, dtype='float32')
+    inner = Conv2D(conv_filters, kernel_size, padding='same',
+                   activation=act, kernel_initializer='he_normal',
+                   name='conv1')(input_data)
+    inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max1')(inner)
+    inner = Conv2D(conv_filters, kernel_size, padding='same',
+                   activation=act, kernel_initializer='he_normal',
+                   name='conv2')(inner)
+    inner = MaxPooling2D(pool_size=(pool_size, pool_size), name='max2')(inner)
+
+    conv_to_rnn_dims = (img_w // (pool_size ** 2), (img_h // (pool_size ** 2)) * conv_filters)
+    inner = Reshape(target_shape=conv_to_rnn_dims, name='reshape')(inner)
+
+    # cuts down input size going into RNN:
+    inner = Dense(time_dense_size, activation=act, name='dense1')(inner)
+
+    # Two layers of bidirecitonal GRUs
+    # GRU seems to work as well, if not better than LSTM:
+    gru_1 = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', name='gru1')(inner)
+    gru_1b = GRU(rnn_size, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='gru1_b')(inner)
+    gru1_merged = add([gru_1, gru_1b])
+        
+  
+    gru_2 = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', name='gru2')(gru1_merged)
+    gru_2b = GRU(rnn_size, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='gru2_b')(gru1_merged)
+    gru2_merged = add([gru_2, gru_2b])
+             
+    
+    gru_3 = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', name='gru3')(gru2_merged)
+    gru_3b = GRU(rnn_size, return_sequences=True, go_backwards=True, kernel_initializer='he_normal', name='gru3_b')(gru2_merged)
+  
+    #**********    
+    inner = Dense(tiger_train.get_output_size(), kernel_initializer='he_normal',
+                  name='dense2')(concatenate([gru_3, gru_3b]))  
+    
+    y_pred = Activation('softmax', name='softmax')(inner)
+    
     Model(inputs=input_data, outputs=y_pred).summary()
     
     labels = Input(name='the_labels', shape=[tiger_train.max_text_len], dtype='float32')
